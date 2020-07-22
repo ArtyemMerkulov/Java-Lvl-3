db1dd58592f43340547e10abd852fd437b9fdcbf
ArtyemMerkulov <49802851+ArtyemMerkulov@users.noreply.github.com>
2020-07-22 10:36:55 +0700

    Update Box.java

--- lesson1/Box.java
+++ lesson1/Box.java
@@ -6,21 +6,45 @@ import java.util.List;
 public class Box<T extends Fruit> {
 
     private ArrayList<T> box;
+    private float weight;
 
     public Box() {
         box = new ArrayList<>(0);
+        weight = 0;
     }
 
     public Box(Box box) {
         shiftTo(box);
     }
 
+    public void shiftTo(Box box) {
+        if (this == box) return;
+
+        for (T e : this.box) box.add(e);
+        clear();
+    }
+
     public void add(T e) {
         box.add(e);
+        weight += e.getWeight();
+    }
+
+    public void clear() {
+        box.clear();
+        box.ensureCapacity(0);
+        weight = 0;
     }
 
     public T get(int pos) {
         return box.get(pos);
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public int getTotalElements() {
+        return box.size();
     }
 
     public List<T> getElements(int beginPos, int n) {
@@ -31,28 +55,8 @@ public class Box<T extends Fruit> {
         return getElements(0, box.size());
     }
 
-    public float getWeight() {
-        return box.stream().mapToInt(e -> e.getWeight()).sum();
-    }
-
-    public int getTotalElements() {
-        return box.size();
-    }
-
     public T removeElements(int index) {
         return box.remove(index);
-    }
-
-    public void clear() {
-        box.clear();
-        box.ensureCapacity(0);
-    }
-
-    public void shiftTo(Box box) {
-        if (this == box) return;
-
-        for (T e : this.box) box.add(e);
-        this.clear();
     }
 
     @Override
@@ -60,7 +64,7 @@ public class Box<T extends Fruit> {
         if (this == anObject) return true;
 
         if (anObject instanceof Box)
-            return this.getWeight() == ((Box) anObject).getWeight();
+            return getWeight() == ((Box) anObject).getWeight();
 
         return false;
     }
